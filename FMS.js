4acf6844593e2afe3244f4eb6042081fdd50628e
Flo <flodef@free.fr>
2017-11-28 17:34:21 +0100

    Values should have fixed float number.

--- FMS.js
+++ FMS.js
@@ -118,7 +118,7 @@
         var num = rest/price;
         var bonus = num > 0 ? Math.floor(num) : Math.ceil(num);
         var rebal = toValue(GLOBAL.invest[index][13]) + bonus;
-        var prov = rebal * price;
+        var prov = (rebal * price).toFixed(2);
         var action = prov > 0;
 
         var array = [];
