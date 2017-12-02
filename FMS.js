1b074b2225ab48be65c33a609008a11063e2e1fc
Flo <flodef@free.fr>
2017-12-02 14:37:07 +0100

    Default value for searchInput

--- FMS.js
+++ FMS.js
@@ -724,7 +724,7 @@
          + '<div class="slider round"></div></label><span class="tooltiptext">' + tooltip + '</span></div></td></tr></table>'
          + '<td colspan="' + colspan + '" align="right">'
          + '<input id="' + id + 'Search" class="searchInput" type="text" placeholder="Search"'
-         + 'onkeyup="searchTable(\'' + id + '\');" value="' + $('#' + id + 'Search').val() + '"></tr></table>'
+         + 'onkeyup="searchTable(\'' + id + '\');" value="' + ($('#' + id + 'Search').val() || "") + '"></tr></table>'
          + getMainTableHead(id);
   }
 
