1f012d706cf425b68993d78dae9c00db7c0b4fbd
Florian <flodef@free.fr>
2019-11-12 12:04:15 +0100

    APP : Add Dist gap (ratio column) to the transaction profit + update ss model

--- FiMS.js
+++ FiMS.js
@@ -600,12 +600,12 @@
       tableHTML += i==0 ? '<tr>' : '<tr title="' + contents[i][0] + '"' +
         (bgcolor ? 'style="background-color:' + bgcolor + ';color:' + color + ';font-weight:bold;"' : '') + '>';
       //for (var j = 0; j < contents[i].length; ++j)
-      for (var j of [7, 10, 12, 14, 18, 19, 22, 32, 23, 29, 33, 36]) {   // Select only the interesting columns
+      for (var j of [7, 10, 12, 14, 18, 19, 22, 32, 23, 29, 34, 36]) {   // Select only the interesting columns
         // Name = 7, Shares = 10, Price = 12, Sell = 14, Rebalance = 18, Provision = 19, Tendency = 22,
         // Daily result	Rate	Dividend	Rate	Stock	Rate	Total	Rate = 23 to 29, Trans profit = 32,
-        // Avg price = 33, Avg gap = 34, Avg lm price = 36, Avg lm progress = 37
+        // Dist gap = 33, Avg price = 34, Avg gap = 35, Avg lm price = 36, Avg lm progress = 37
         var con =  i == 0 || j != 12
-                    ? i == 0 || j < 23 || j == 32
+                    ? i == 0 || j < 23
                       ? contents[i][j]
                       : (contents[i][j] ? toCurrency(contents[i][j], 3) : "") + ' (' + contents[i][j+1] + ')'
                     : contents[i][12]
