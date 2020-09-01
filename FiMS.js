5992467d7b43e3117dacbedc01a7c6e23d77c426
Florian <flodef@free.fr>
2020-09-02 00:08:50 +0200

    APP : Add next div dur to Investment table

--- FiMS.js
+++ FiMS.js
@@ -15,7 +15,7 @@
   GLOBAL.settings = "settings";
   GLOBAL.account = "account";
   GLOBAL.dashboardFormula = "Dashboard!A:B";
-  GLOBAL.investmentFormula = "Investment!A:AL";
+  GLOBAL.investmentFormula = "Investment!A:AT";
   GLOBAL.historicFormula = restrainFormula("Historic!A:J");
   GLOBAL.evolutionFormula = restrainFormula("Evolution!A:J");
   GLOBAL.resultFormula = "Result!A:H";
@@ -600,12 +600,12 @@
       tableHTML += i==0 ? '<tr>' : '<tr title="' + contents[i][0] + '"' +
         (bgcolor ? 'style="background-color:' + bgcolor + ';color:' + color + ';font-weight:bold;"' : '') + '>';
       //for (var j = 0; j < contents[i].length; ++j)
-      for (var j of [7, 10, 12, 14, 18, 19, 22, 32, 23, 29, 34, 36]) {   // Select only the interesting columns
+      for (var j of [7, 10, 12, 14, 18, 19, 22, 32, 23, 29, 34, 36, 45]) {   // Select only the interesting columns
         // Name = 7, Shares = 10, Price = 12, Sell = 14, Rebalance = 18, Provision = 19, Tendency = 22,
         // Daily result	Rate	Dividend	Rate	Stock	Rate	Total	Rate = 23 to 29, Trans profit = 32,
-        // Dist gap = 33, Avg price = 34, Avg gap = 35, Avg lm price = 36, Avg lm progress = 37
+        // Dist gap = 33, Avg price = 34, Avg gap = 35, Avg lm price = 36, Avg lm progress = 37, Next div dur = 45
         var con =  i == 0 || j != 12
-                    ? i == 0 || j < 23
+                    ? i == 0 || j < 23 || j > 37
                       ? contents[i][j]
                       : (contents[i][j] ? toCurrency(contents[i][j], 3) : "") + ' (' + contents[i][j+1] + ')'
                     : contents[i][12]
