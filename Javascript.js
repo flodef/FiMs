cc44b209677b266fd82a7d7ef894429969cc26aa
Florian <flodef@free.fr>
2020-11-11 01:54:32 +0100

    APP : Add a new associate app (html + js) APP : Add a common javascript library for main and associate apps APP : javascript.js can now automatically select which app to launch from url

--- Javascript.js
+++ Javascript.js
@@ -4,12 +4,16 @@ var src = ["Lib/jquery.min.js",
            "Lib/jquery.csv-0.71.min.js",
            "Lib/sorttable.js",
            "Lib/xlsx.full.min.js",
+           "Common.js",
            "FiMS.js",
            "Google.js",
          ];
 
+var url = document.URL.split('/');
+var page = url[url.length-1].split('.')[0];
+
 src.forEach(element => {
   var imp = document.createElement('script');
-  imp.src = element;
+  imp.src = page == "index" ? element : element.replace("FiMS", "Associate");
   document.head.appendChild(imp);
 });
