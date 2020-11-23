fe7c90237475f43b6e1e1be91c03e29c2be2c9db
Florian <flodef@free.fr>
2020-11-23 20:24:39 +0100

    APP : Library full update + link

--- Javascript.js
+++ Javascript.js
@@ -1,9 +1,9 @@
-var src = ["Lib/jquery.min.js",
-           "Lib/jquery-ui.js",
-           // "Lib/jquery.easy-autocomplete.min.js",
-           "Lib/jquery.csv-0.71.min.js",
-           "Lib/sorttable.js",
-           "Lib/xlsx.full.min.js",
+var src = ["Lib/jquery.min.js",                       // https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
+           "Lib/jquery-ui.js",                        // https://code.jquery.com/ui/1.12.1/jquery-ui.js
+           "Lib/jquery.csv.min.js",                   // https://raw.githubusercontent.com/typeiii/jquery-csv/master/src/jquery.csv.min.js
+           "Lib/sorttable.js",                        // https://www.kryogenix.org/code/browser/sorttable/sorttable.js
+           // "Lib/jquery.easy-autocomplete.min.js",  // https://raw.githubusercontent.com/pawelczak/EasyAutocomplete/master/dist/jquery.easy-autocomplete.min.js
+           "Lib/xlsx.full.min.js",                    // https://raw.githubusercontent.com/SheetJS/sheetjs/master/dist/xlsx.full.min.js
            "Google.js",
            "Common.js",
            "FiMS.js",
