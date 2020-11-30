d1ca0c11343ff3ae2acb664c1ab76d00f3704942
Florian <flodef@free.fr>
2020-12-01 00:41:27 +0100

    APP : Merge Web apps together so WebApp Associate is not needed anymore

--- GoogleAppsScript - WebApp Associate/Deploy.bat
+++
@@ -1,4 +0,0 @@
-@ECHO off
-REM AKfycbw1nj4Vi29hGeU9Ju74r_hTfX1ZwwsJiW86ygvqguyQ @HEAD
-REM AKfycbxNa5DAp8OWPUXxJXA01dwHBNdzxQx7IhPLnMBJlHkxDCjN-Xd_ @12 - web app meta-version
-START /B /WAIT clasp deploy --deploymentId AKfycbxNa5DAp8OWPUXxJXA01dwHBNdzxQx7IhPLnMBJlHkxDCjN-Xd_
--- GoogleAppsScript - WebApp Associate/Push.bat
+++
@@ -1,3 +0,0 @@
-@ECHO off
-ECHO Once finished, use 'clasp push'
-START /B /WAIT clasp setting scriptId 1yTGSYR2MfS4vX8XO9fLrnmrgGeMObnrOu_3EZp-KzXRAhLMBoJ8HX3LA
