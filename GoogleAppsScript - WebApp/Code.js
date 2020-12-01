3ea955ff97f16187206a8ec69e54c1784861d69d
Flo <flodef@free.fr>
2020-12-01 21:43:06 +0100

    SS : Simplify server side (one page instead of - Main/Associate => loading in JavaScript with Google.script.run.getProperty(fileName))

--- GoogleAppsScript - WebApp/Code.js
+++ GoogleAppsScript - WebApp/Code.js
@@ -15,11 +15,10 @@ function doGet(e) {
   const isMain = userId == "Flodef";
   const favIcon = 'https://raw.githubusercontent.com/flodef/FiMS/master/Img/Favicon2.png';
   const pageTitle = isMain ? 'FiMs Main' : 'FiMs Associate';
-  const fileName = !workInProgress ? isMain ? 'Main' : 'Associate' : 'WorkInProgress';
+  const fileName = !workInProgress ? 'Index' : 'WorkInProgress';
   const spreadsheetId = isMain ? '1JJ7zW4GD7MzMBTatntdnojX5bZYcqI1kxMWIvc0_LTw' : '1pMnJel8OYtwk1Zu4YgTG3JwmTA-WLIMf6OnCQlSgprU';
 
   setProperty("userId", userId);
-  setProperty("fileName", fileName);
   setProperty("pageTitle", pageTitle);
   setProperty("spreadsheetId", spreadsheetId);
 
