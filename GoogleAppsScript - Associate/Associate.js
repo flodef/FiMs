27ccdff734bdee5979da644c02ef72cd87e19577
Flo <flodef@free.fr>
2022-04-28 01:27:06 +0200

    SS : Add Common library to all Google Apps scripts SS : Update Associate apps script to new Common library

--- GoogleAppsScript - Associate/Associate.js
+++ GoogleAppsScript - Associate/Associate.js
@@ -1,9 +1,8 @@
-/* global SpreadsheetApp, GmailApp */
-/* exported updateAssociate, sendCharity, SHEETNAME */
+/* global GmailApp, FR, FC, SS, FM, _getSheet, _isCurrentMonth, _copyFirstRow,
+_sendMessage, _toDate, _insertFirstRow, _round*/
+/* exported updateAssociate, sendCharity */
 
 
-// MAIN SPREADSHEET
-const SS = SpreadsheetApp.getActiveSpreadsheet();
 
 // ASSOCIATE COLS
 const ASSNAME_COL = 2;          // Should be the "ID" column
@@ -18,11 +17,6 @@ const ASSOCIATE = 'Associate';      // The "Associate" sheet name
 const ASSMODEL = 'AssociateModel';  // The "AssociateModel" sheet name
 const ASSHISTO = 'AssociateHistoric';  // The "AssociateHistoric" sheet name
 
-// VARIOUS
-const MAIL = 'fdefroco@gmail.com';
-const FR = 2;     // First Row
-const FC = 1;     // First Column
-const FM = 0;     // First month (january)
 
 
 function updateAssociate() {
@@ -123,151 +117,4 @@ function sendCharity() {
     recap += '\nTOTAL = ' + _round(total, 2, ' €');
     _sendMessage(object, recap);
   }
-}
-
-
-function SHEETNAME() {
-  return SpreadsheetApp.getActiveSheet().getName();
-}
-
-// ************************************
-// ***** COMMON LIBRARY FUNCTIONS *****
-// ************************************
-
-function _getSheet(sheetName) {
-//  var sheet = SpreadsheetApp.getActiveSheet();
-//  return sheet && sheet.getName() == sheetName
-//  ? sheet
-//  : SpreadsheetApp.setActiveSheet(SpreadsheetApp.getActiveSpreadsheet().getSheetByName(sheetName));
-  return SS.getSheetByName(sheetName);
-}
-
-function _setRangeValues(sheet, row, column, data) {
-  if (data) {
-    sheet.getRange(row, column, data.length, data[0].length).setValues(data);
-  }
-}
-
-// function _copyFormula(formula, range) {
-//   formula.copyTo(range, SpreadsheetApp.CopyPasteType.PASTE_FORMULA, false);
-// }
-
-function _insertFirstRow(sheet, data, isFast, lc) {
-  var lr = sheet.getMaxRows();
-  lc = lc ? lc : sheet.getMaxColumns();
-
-  if (lr >= FR) {
-    if (!isFast) {  // Insert a row at the end, then copy all the data with an offset
-      sheet.insertRowAfter(lr);
-      sheet.getRange(FR, FC, lr-1, lc).copyTo(sheet.getRange(FR + 1, FC));
-    } else {        // Insert a row at the begin, then copy the second row to the first row
-      sheet.insertRowBefore(FR);
-      sheet.getRange(FR+1, FC, 1, lc).copyTo(sheet.getRange(FR, FC));
-    }
-  } else {
-    sheet.insertRowAfter(lr);
-    sheet.getRange(lr+1, FC, 1, lc).clearFormat();
-  }
-
-  _setRangeValues(sheet, FR, FC, data);
-}
-
-function _copyFirstRow(sheet, array) {
-  if (!_isCurrentDay(array)) {
-    _insertFirstRow(sheet, null, true);
-    _setRangeValues(sheet, FR + 1, FC, [array[0]]);    // Copy only values into previous row (archive)
-  }
-}
-
-// function _isMarketOpen() {
-//   var x = new Date();
-//   var d = x.getDay();
-//   var h = x.getHours();
-//
-//   return d >= FD && d <= LD && h >= FH && h <= LH;
-// }
-
-
-function _toFixed(value, precision) {
-  var str = value.toString();
-  str += (str.indexOf('.') != -1 ? '' : '.') + Array(precision+1).join('0');
-  return str.slice(0, str.indexOf('.') + precision + Math.min(precision, 1));
-}
-
-function _round(value, precision, symbol) {
-  var mult = Math.pow(10, precision);
-  var sup = symbol == '%' ? 100 : 1;
-  symbol = typeof(symbol) === 'string' ? symbol : '';
-
-  return _toFixed(Math.round(value * sup * mult) / mult, precision) + symbol;
-}
-
-function _isCurrentDay(array) {
-  return array && array.length > 0 ? _toStringDate() == _toStringDate(array[0][0]) : false;
-}
-
-function _isCurrentMonth(array) {
-  return array && array.length > 0 ? new Date().getMonth() == array[0][0].getMonth() : false;
-}
-
-function _toDate(date) {
-  date = typeof(date) == 'object' ? date : new Date();
-  date.setHours(0, 0, 0, 0);
-  return date;
-}
-
-function _toStringDate(date) {
-  if (typeof(date) == 'string') {
-    return date && date.split('/').length == 3
-      ? date.replace(/(^|\/)0+/g, '$1').split('/')[1] + '/'
-    + date.replace(/(^|\/)0+/g, '$1').split('/')[0] + '/'
-    + date.split('/')[2]
-      : null;
-  } else if (typeof(date) == 'object') {
-    var day = date.getDate();
-    var month = date.getMonth() + 1;
-    var year = date.getFullYear();
-    return month + '/' + day + '/' + year;
-  } else {
-    return _toStringDate(new Date());
-  }
-}
-
-// function _indexOf(array, value, index, start) {
-//   index = index >= 0 ? index : null;
-//   var x = parseInt(start) ? parseInt(start) : 0;
-//
-//   var i = null;
-//   if (Array.isArray(array)) {
-//     while(x < array.length
-//           && ((index == null && array[x] != value)
-//       || (index != null && array[x][index] != value))) { ++x; }
-//
-//     i = x < array.length ? x : null;
-//   }
-//
-//   return i;
-// }
-
-// function _isLoading(value) {
-//   return typeof(value) === 'string' && (value == LOADING || value == '');
-// }
-//
-// function _isError(value) {
-//   return typeof(value) === 'string' && value.substring(0, 1) === '#';
-// }
-//
-// function _archiveMessage(thread, shouldDelete) {
-//   if (thread) {
-//     thread = thread.markRead();
-//     if (shouldDelete) {
-//       thread.moveToTrash();
-//     } else {
-//       thread.moveToArchive();
-//     }
-//   }
-// }
-
-function _sendMessage(object, message) {
-  GmailApp.sendEmail(MAIL, object, message);
 }
