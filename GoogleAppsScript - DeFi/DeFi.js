13787fec73e8ead6993cd0a417ae1ac23e7bc461
Flo <flodef@free.fr>
2022-04-29 17:00:41 +0200

    SS : Add update Historic once a month

--- GoogleAppsScript - DeFi/DeFi.js
+++ GoogleAppsScript - DeFi/DeFi.js
@@ -1,17 +1,22 @@
 /* global _getSheet, _deleteOlderThanAYear, _copyFirstRow, _AreRowsDifferent,
-FR, FC */
-/* exported nightlyUpdate */
+_toDate, _isCurrentMonth, FR, FC */
+/* exported nightlyUpdate, monthlyUpdate */
 
 
 
 // SHEET NAMES
 const EVOLUTION = 'Evolution';   // The "Evolution" sheet name
 const PRICE = 'Price';           // The "Price" sheet name
+const HISTORIC = 'Historic';     // The "Historic" sheet name
 
 
 
 function nightlyUpdate() {
   _updateEvolution();
+}
+
+function monthlyUpdate() {
+  _updateHistoric();
 }
 
 
@@ -32,5 +37,18 @@ function _updateEvolution() {
     sheet = _getSheet(EVOLUTION);
     array = sheet.getSheetValues(FR, FC, 1, -1);
     _copyFirstRow(sheet, array);
+  }
+}
+
+function _updateHistoric() {
+  // Search if entries have already been added
+  const sheet = _getSheet(HISTORIC);
+  const array = sheet.getSheetValues(FR, FC, 2, -1);
+  if (!_isCurrentMonth(array)) {
+    _copyFirstRow(sheet, array);
+
+    const date = _toDate();                 // Get date without hours
+    date.setMonth(date.getMonth() + 1, 1);  // Set the date at the first of next month
+    sheet.getRange(FR+1, FC).setValue(date);
   }
 }
