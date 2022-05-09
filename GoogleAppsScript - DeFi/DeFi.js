310bfafd95018a5707df5ec1876e1f0d38d878b1
Flo <flodef@free.fr>
2022-05-09 10:29:59 +0200

    BUG : update historic was setting the next month instead of the previous one

--- GoogleAppsScript - DeFi/DeFi.js
+++ GoogleAppsScript - DeFi/DeFi.js
@@ -120,8 +120,8 @@ function _updateHistoric() {
   if (!_isCurrentMonth(array)) {
     _copyFirstRow(sheet, array);
 
-    const date = _toDate();                 // Get date without hours
-    date.setMonth(date.getMonth() + 1, 1);  // Set the date at the first of next month
-    sheet.getRange(FR+1, FC).setValue(date);// Update the previous month date
+    const date = _toDate();                   // Get date without hours
+    date.setMonth(date.getMonth()-1, 1);      // Set the date at the first of previous month
+    sheet.getRange(FR+1, FC).setValue(date);  // Update the previous month date
   }
 }
