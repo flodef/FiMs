2e8efe5d793bb986897008c2f5ae04466f3be96d
Flo <flodef@free.fr>
2022-06-13 02:39:43 +0200

    PAY: Generate translation button + label autotically from ss PAY: Open wallet instead of copy only merchant address PAY: Step button allows to go back / forward to previous / next step PAY: Add help to step 2 + 3 (not implemented yet) BUG: Retry go back to step 2 instead of 1

--- Pay.js
+++ Pay.js
@@ -1,7 +1,7 @@
 /* global GLOBAL, $, initCommon, google, getDiv, finishLoading, 
 getValue, setValue, displayError, getImage, getDataValue, showLoader, 
 getMainTitle, toCurrency, toStringDate, translate, displayElement, 
-setTranslationLanguage */
+setTranslationLanguage, indexOf */
 /* exported init, onKeyUp, validatePayment, getButtonAction */
 
 GLOBAL.hasTranslation = true;
@@ -17,6 +17,7 @@ GLOBAL.customerAddressFormula = "Check!D";
 
 GLOBAL.solanaAddressPattern = /^[1-9A-HJ-NP-Za-km-z]{32,44}$/i;
 
+GLOBAL.currentStep = 0;
 GLOBAL.customerAddress = "";
 GLOBAL.merchantAddress = "";
 GLOBAL.attempt = 0; // Number of attempt to load payment data
@@ -29,29 +30,37 @@ GLOBAL.messages = {
   invalidAddress: "Invalid Solana Address!",
   invalidPayment:
     "Error: Your payment could not been verified. Try to pay by another mean. We will proceed to a refund shortly.",
+  help: "Help",
   duration: "($ ago)",
 };
+
+GLOBAL.helpURL = "https://fims.fi?id=Help";
 
 GLOBAL.processStep = [
   {
     index: 1,
-    label: "Choisissez votre langue /<br>Choose your language:",
-    button: "Français,English",
+    label: "Choose your language:",
+    button: "English",
     action: setTranslationLanguage.name,
   },
   {
     index: 2,
     label: "Open your Solana wallet and send the payment to the merchant:",
-    button: "Copy Merchant Address",
-    action: copyMerchantAddress.name,
+    button: "Wallet",
+    action: openWallet.name,
   },
   {
     index: 3,
-    label: "Copy your Solana wallet address then verify payment:",
-    button: "Verify Payment",
-    action: validatePayment.name,
+    label: "Copy your Solana wallet address from your wallet then verify payment:",
+    button: "Verify Payment," + GLOBAL.messages.help,
+    action: verifyPayment.name,
   },
 ];
+
+GLOBAL.wallet = [];
+GLOBAL.wallet["Exodus"] = "https://play.google.com/store/apps/details?id=exodusmovement.exodus";
+GLOBAL.wallet["Solflare"] = "https://solflare.com/portfolio";
+GLOBAL.wallet[GLOBAL.messages.help] = "";
 
 GLOBAL.header = {
   cryptoAddress: "Crypto Address",
@@ -93,6 +102,8 @@ function displayContent(id, contents) {
   GLOBAL.merchantAddress = getDataValue(contents, GLOBAL.header.cryptoAddress);
   const company = getDataValue(contents, GLOBAL.header.company);
   const merchantTitle = getMainTitle(company);
+  setLanguages();
+  setWallet();
 
   const logo = getImage(GLOBAL.user.ID, "Pay/Merchant", [{ name: "style", value: "margin: 25px;" }]);
   const logoHTML = getDiv("logo", null, "center", logo);
@@ -110,6 +121,31 @@ function displayContent(id, contents) {
   displayElement("#mainContent, #merchant, #mainHeading", true, 3000);
 }
 
+function setLanguages() {
+  const step = GLOBAL.processStep[0];
+  setTranslationLanguage(step.button);
+
+  const translation = GLOBAL.data[GLOBAL.translation];
+  const index = indexOf(translation, step.label, GLOBAL.translationIndex);
+
+  const label = [];
+  translation[index].forEach((x) => label.push(x));
+  step.label = label.join("<br>");
+
+  const button = [];
+  translation[0].forEach((x) => button.push(x));
+  step.button = button.join(",");
+}
+
+function setWallet() {
+  const step = GLOBAL.processStep[1];
+  const wallet = [];
+  for (let x in GLOBAL.wallet) {
+    wallet.push(x);
+  }
+  step.button = wallet.join(",");
+}
+
 function setProcess() {
   let numberTML = "";
   GLOBAL.processStep.forEach((step) => {
@@ -117,7 +153,8 @@ function setProcess() {
     numberTML += getImage(stepNumber, "Button", [
       { name: "id", value: stepNumber },
       { name: "class", value: "stepNumber" },
-      { name: "style", value: "margin: 25px; width: 150px; height: 150px; opacity: 0.25;" },
+      { name: "style", value: "margin: 25px; width: 150px; height: 150px; opacity: 0.25; cursor:pointer" },
+      { name: "onclick", value: "setCurrentStep(" + step.index + ")" },
     ]);
   });
   numberTML = getDiv("number", null, null, numberTML);
@@ -159,38 +196,60 @@ function getStepButton(index, label, action) {
 }
 
 async function getButtonAction(action, value, index) {
-  try {
-    await action.call(this, value);
+  const isOk = await action.call(this, value);
+  if (isOk || isOk === undefined) {
     setCurrentStep(index + 1);
-  } catch (e) {
-    displayError(e);
   }
 }
 
 function setCurrentStep(index) {
-  $("#step").html(getStepContent(index - 1));
-  displayElement("#step", false, 0);
-  displayElement("#step", true, 2000);
+  if (index !== GLOBAL.currentStep) {
+    GLOBAL.currentStep = index;
+    $("#step").html(getStepContent(index - 1));
+    displayElement("#step", false, 0);
+    displayElement("#step", true, 2000);
   
-  displayElement("#number", true, 0);
-  $(".stepNumber").fadeTo(1000, 0.25);
-  $("#number" + index).fadeTo(1000, 1);
+    displayElement("#number", true, 0);
+    $(".stepNumber").fadeTo(1000, 0.25);
+    $("#number" + index).fadeTo(1000, 1);
+  }
 }
 
-function copyMerchantAddress() {
+function openHelp(section) {
+  const helpURL = GLOBAL.helpURL + "#" + section;
+  window.open(helpURL, "_blank");
+}
+
+function openWallet(wallet) {
   navigator.clipboard.writeText(GLOBAL.merchantAddress);
+  const url = GLOBAL.wallet[wallet];
+  if (url) {
+    window.open(url, "_blank");
+    return true;
+  } else {
+    openHelp(arguments.callee.name);
+    return false;
+  }
 }
 
-async function validatePayment() {
-  const clipboard = await navigator.clipboard.readText();
-  const customerAddress = clipboard.match(GLOBAL.solanaAddressPattern);
-  if (customerAddress && customerAddress.length == 1 && customerAddress != GLOBAL.merchantAddress) {
-    showLoader(true);
-    GLOBAL.retry = 0;
-    GLOBAL.customerAddress = customerAddress[0];
-    setCustomerAddress(GLOBAL.customerAddress, displayPaymentStatus);
+async function verifyPayment(option) {
+  if (option != translate(GLOBAL.messages.help)) {
+    const clipboard = await navigator.clipboard.readText();
+    const customerAddress = clipboard.match(GLOBAL.solanaAddressPattern);
+    if (customerAddress && customerAddress.length == 1 && customerAddress != GLOBAL.merchantAddress) {
+      showLoader(true);
+      displayElement("#process", false, 0);
+      GLOBAL.retry = 0;
+      GLOBAL.customerAddress = customerAddress[0];
+      setCustomerAddress(GLOBAL.customerAddress, displayPaymentStatus);
+      return true;
+    } else {
+      displayError(GLOBAL.messages.invalidAddress);
+      return false;
+    }
   } else {
-    throw GLOBAL.messages.invalidAddress;
+    openHelp(arguments.callee.name);
+    return false;
   }
 }
 
@@ -249,13 +308,14 @@ async function displayPaymentStatus(id, contents) {
         html += getMainTitle(translate(GLOBAL.messages.duration).replace("$", duration));
       }
     } else {
-      html += getStepButton(0, "Retry", setProcess.name);
+      html += getStepButton(1, "Retry", setProcess.name);
     }
-
-    $("#process").html(html);
-
+    
     setCustomerAddress("");
     showLoader(false);
+
+    $("#process").html(html);
+    displayElement("#process", true);
   }
 }
 
